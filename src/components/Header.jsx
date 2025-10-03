a5f64f98fe775e797351cc59209fcd3a6ef9499c
ivan-pardo <yvanpardoalcocer@gmail.com>
2025-10-03 20:10:55 -0300

    change: elementos a por Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom"
+
 const Header = () => {
   return (
     <header style={{background: "gray"}}>
       <img src="https://cdn.pixabay.com/photo/2016/03/22/09/08/online-store-1272390_1280.png" width={400} alt="logo-tienda" />
       <nav>
         <ul>
-          <li><a href="/">Inicio</a></li>
-          <li><a href="/dashboard">Dashboard</a></li>
-          <li><a href="/login">Login</a></li>
-          <li><a href="/register">Register</a></li>
+          <li><Link to="/">Inicio</Link></li>
+          <li><Link to="/dashboard">Dashboard</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/register">Register</Link></li>
           <button>Cerrar Sesion</button>
         </ul>
       </nav>
