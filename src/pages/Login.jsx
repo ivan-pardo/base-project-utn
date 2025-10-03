36f8c9df57b2269ee5548a19eab936e85ab9d7d1
ivan-pardo <yvanpardoalcocer@gmail.com>
2025-10-03 20:32:17 -0300

    add: contenido estatico en login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,23 @@ import { Layout } from "../components/Layout"
 const Login = () => {
   return (
     <Layout>
-      <h1>HoLA desde LoGIN!</h1>
+      <h1>Inicia Sesion</h1>
+    
+      <section>
+        <h2>Hola, bienvenido de nuevo</h2>
+        <form action="">
+          <div>
+            <label>Correo Electronico:</label>
+            <input type="email" name="email" />
+          </div>
+
+          <div>
+            <label>Contraseña:</label>
+            <input type="password" name="password" />
+          </div>
+          <button>Ingresar</button>
+        </form>
+      </section>
     </Layout>
   )
 }
