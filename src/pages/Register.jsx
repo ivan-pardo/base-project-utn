81b2cdb9c863fdf33d4f2590abae82d9fe91dcd0
ivan-pardo <yvanpardoalcocer@gmail.com>
2025-10-03 20:36:33 -0300

    add: contenido estatico en register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,28 @@ import { Layout } from "../components/Layout"
 const Register = () => {
   return (
     <Layout>
-      <h1>HOla desde Register</h1>
+      <h1>Registrate</h1>
+    
+      <section>
+        <h2>Hola, Bienvenido</h2>
+        <form action="">
+          <div>
+            <label>Username:</label>
+            <input type="text" />
+          </div>
+
+        <div>
+            <label>Correo Electronico:</label>
+            <input type="email" />
+        </div>
+
+          <div>
+            <label>Contraseña:</label>
+            <input type="password" />
+          </div>
+          <button>Ingresar</button>
+        </form>
+      </section>
     </Layout>
   )
 }
