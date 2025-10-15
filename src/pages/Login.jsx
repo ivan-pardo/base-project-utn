ce733fd689e21a03bd4d456b01b2e7d5f0244ff4
ivan-pardo <yvanpardoalcocer@gmail.com>
2025-10-15 19:18:21 -0300

    add: contenido login y notfound

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,6 @@ const Login = () => {
   return (
     <Layout>
       <h1>Inicia Sesion</h1>
-    
       <section>
         <h2>Hola, bienvenido de nuevo</h2>
         <form action="">
@@ -12,7 +11,6 @@ const Login = () => {
             <label>Correo Electronico:</label>
             <input type="email" name="email" />
           </div>
-
           <div>
             <label>Contraseña:</label>
             <input type="password" name="password" />
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom"
+import { Home } from "./Home"
+
 const NotFound = () => {
   return (
     <Layout>
-      <h1>HolA desed NOTfound</h1>
+      <h1>Pagina no encontrada</h1>
+      <p>Lo sentimos, la pagina que estas buscando no existe.</p>
+      <p>Verifica la URL o volve al inicio.</p>
+      <Link to="/">Ir a Inicio</Link>
     </Layout>
   )
 }
 
-export {NotFound }
+export { NotFound }
