cae3cdb7de34f16da937e0e6de981ce3b712af19
ivan-pardo <yvanpardoalcocer@gmail.com>
2025-10-17 19:28:32 -0300

    add: agregando funciones en home y header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,36 @@
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [user, setUser] = useState(true);
+
   return (
-    <header style={{background: "gray"}}>
-      <img src="https://cdn.pixabay.com/photo/2016/03/22/09/08/online-store-1272390_1280.png" width={400} alt="logo-tienda" />
+    <header style={{ background: "gray" }}>
+      <img
+        src="https://cdn.pixabay.com/photo/2016/03/22/09/08/online-store-1272390_1280.png"
+        width={400}
+        alt="logo-tienda"
+      />
       <nav>
         <ul>
-          <li><Link to="/">Inicio</Link></li>
-          <li><Link to="/dashboard">Dashboard</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Register</Link></li>
-          <button>Cerrar Sesion</button>
+          {user && <>
+              <li><Link to="/">Inicio</Link></li>
+              <li><Link to="/dashboard">Dashboard</Link></li>
+              <button onClick={() => setUser(false)}>Cerrar Sesion</button>
+            </>
+          }
+          {
+            !user && <>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/register">Register</Link></li>
+            
+            </>
+          }
+
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export { Header }
+export { Header };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,74 @@
-import { Layout } from "../components/Layout"
+import { useEffect, useState } from "react";
+import { Layout } from "../components/Layout";
 
 const Home = () => {
-  return (
-    <Layout>
-      <section>
-        <h1>Bienvenido a Nuestra Tienda</h1>
-        <p>Descubri una seleccion exclusiva de productos para vos. Calidad, confianza y atencion personalizada.</p>
-      </section>
-      <section>
-        <h2>¿Por que elegirnos?</h2>
-        <ul>
-          <li>
-            <h3>Envios a todo el pais</h3>
-            <p>Recibi tu compra en la puerta de tu casa estes donde estes.</p>
-          </li>
-          <li>
-            <h3>Pagos Seguros</h3>
-            <p>Trabajamos con plataformas que garantizan tu seguridad</p>
-          </li>
-          <li>
-            <h3>Atencion personalizada</h3>
-            <p>Estamos disponibles para ayudarte en todo momento</p>
-          </li>
-        </ul>
-      </section>
 
-      <section>
-        <h2>Nuestros productos</h2>
-        <p>Elegi entre nuestras categorias mas populares</p>
+  const [products, setProducts] = useState([]);
 
-        <div>
-          <p>Cargando productos...</p>
-        </div>
-      </section>
-    </Layout>
-  )
-}
+  const fetchingProducts = async () => {
+    const response = await fetch("https://fakestoreapi.com/products", {method: "GET"});
+    const data = await response.json();
+    setProducts(data);
+  };  
+  
+  useEffect(() => {
+    fetchingProducts();
+  }, []);
 
-export { Home } 
+  const handleDelete = (id) => {
+    console.log("Borrando producto", id)
+  }
+
+    return (
+      <Layout>
+        <section>
+          <h1>Bienvenido a Nuestra Tienda</h1>
+          <p>
+            Descubri una seleccion exclusiva de productos para vos. Calidad,
+            confianza y atencion personalizada.
+          </p>
+        </section>
+        <section>
+          <h2>¿Por que elegirnos?</h2>
+          <ul>
+            <li>
+              <h3>Envios a todo el pais</h3>
+              <p>Recibi tu compra en la puerta de tu casa estes donde estes.</p>
+            </li>
+            <li>
+              <h3>Pagos Seguros</h3>
+              <p>Trabajamos con plataformas que garantizan tu seguridad</p>
+            </li>
+            <li>
+              <h3>Atencion personalizada</h3>
+              <p>Estamos disponibles para ayudarte en todo momento</p>
+            </li>
+          </ul>
+        </section>
+
+        <section>
+          <h2>Nuestros productos</h2>
+          <p>Elegi entre nuestras categorias mas populares</p>
+          <div>
+            <p>Cargando productos...</p>
+          </div>
+        </section>
+        {
+          products.map((product) => <div key={product.id}>
+            <h2>{product.title}</h2>
+            <img src={product.image} alt={product.title} width={150} />
+            <p>{product.description}</p>
+            <h3>${product.price}</h3>
+            <div>
+              <button>Comprar</button>
+              <button onClick={() => handleDelete(product.id)}>Borrar</button>
+            </div>
+            <hr/>
+            <br/>
+          </div>)
+        }
+      </Layout>
+    );
+};
+
+export { Home }
