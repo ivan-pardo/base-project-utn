7ff6782c54a1be87ad433a02fdf09b9327dcd237
ivan-pardo <yvanpardoalcocer@gmail.com>
2025-10-17 20:16:09 -0300

    feat: creando nuevo producto en dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,26 +1,44 @@
+import { useState } from "react"
 import { Layout } from "../components/Layout"
 
 const Dashboard = () => {
+
+  const [name, setName] = useState();
+  const [price, setPrice] = useState();
+  const [description, setDescription] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newProduct = {
+      id: crypto.randomUUID(),
+      name: name,
+      price: price,
+      description: description
+    }
+
+    console.log(newProduct, "Enviando Form... ");
+  }
+
   return (
     <Layout>
       <h1>Panel de Administracion</h1>
       
       <section>
         <h2>Cargar nuevo producto</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label>Nombre del producto:</label>
-            <input type="text" name="nombre"/>
+            <input type="text" name="nombre" onChange={(e) => setName(e.target.value)}/>
           </div>
 
           <div>
             <label>Precio:</label>
-            <input type="number" name="precio"/>
+            <input type="number" name="precio" onChange={(e) => setPrice(e.target.value)}/>
           </div>
 
           <div>
             <label>Descripcion:</label>
-            <textarea type="descripcion" rows="4"/>
+            <textarea type="descripcion" rows="4" onChange={(e) => setDescription(e.target.value)}/>
           </div>
 
           <button>Guardar producto</button> 
